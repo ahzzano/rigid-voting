c3b32cda966a8807cfef412a9c6c514e19c44234
Will Santos <w.santos.gg@gmail.com>
2025-05-11 22:29:58 +0800

    feat: add login form component

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { enhance } from "$app/forms"; // Import the enhance action
+</script>
+
+Login
+<form method="POST" use:enhance>
+    <label>
+        Email
+        <input class="input" name="email" required />
+    </label>
+    <label>
+        Password
+        <input class="input" name="password" type="password" required />
+    </label>
+    <button class="btn">signup</button>
+</form>
