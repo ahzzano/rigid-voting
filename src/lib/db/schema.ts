a5cd525945a2fbd3c0553121eff12ddbb0c96459
Will Santos <w.santos.gg@gmail.com>
2025-05-11 23:33:49 +0800

    feat: add more user info

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -3,18 +3,31 @@ import type { InferSelectModel } from "drizzle-orm";
 
 
 export const users = pgTable('users', {
-    id: serial('id').primaryKey(),
+    id: serial('id')
+        .primaryKey(),
     email: text('email'),
     password: text('password')
 })
 
 export const sessions = pgTable('sessions', {
     id: text('id').primaryKey(),
-    userId: serial('user_id').notNull().references(() => users.id),
+    userId: serial('user_id')
+        .notNull()
+        .references(() => users.id),
     expiresAt: timestamp('expires_at', {
         mode: "date",
         withTimezone: true
     }).notNull()
+})
+
+export const userInfo = pgTable('userInfo', {
+    userId: serial('user_id')
+        .primaryKey()
+        .references(() =>
+            users.id
+        ),
+    firstName: text('firstName'),
+    lastName: text('lastName')
 })
 
 export type User = InferSelectModel<typeof users>;
