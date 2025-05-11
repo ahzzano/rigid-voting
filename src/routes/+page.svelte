91e9e118e49af56810e278b56d4fbf35c415a421
Will Santos <w.santos.gg@gmail.com>
2025-05-11 18:14:47 +0800

    feat: add deps list to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
+
+<div>
+    Currently Installed:
+    <ul>
+        <li>Svelte</li>
+        <li>TailwindCSS</li>
+    </ul>
+</div>
