a101a5325ee5cb989dfca31793a0d92a74c086ca
Will Santos <w.santos.gg@gmail.com>
2025-05-13 10:40:52 +0800

    feat: add getUserData function

---
+++ src/lib/users.ts
@@ -0,0 +1,15 @@
+import { eq } from "drizzle-orm";
+import { db } from "./db/database";
+import { userInfos } from "./db/schema";
+
+export async function getUserInfo(userId: number) {
+    const res = await db.select({ userInfo: userInfos })
+        .from(userInfos)
+        .where(eq(userInfos.userId, userId))
+
+    if (res.length < 1) {
+        return null
+    }
+
+    return res[0].userInfo
+}
