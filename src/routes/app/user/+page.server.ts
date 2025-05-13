53042835a8cab1ce3539f560cad67a71fff6c51e
Will Santos <w.santos.gg@gmail.com>
2025-05-13 10:40:52 +0800

    feat: add user info form to /app/user

--- src/routes/app/user/+page.server.ts
+++ src/routes/app/user/+page.server.ts
@@ -1,8 +1,29 @@
+import type { User } from "$lib/db/schema";
 import { readUserData } from "$lib/userData";
+import { getUserInfo } from "$lib/users";
+import type { Actions } from "@sveltejs/kit";
 
 export async function load({ cookies }) {
-    const userData = readUserData(cookies)
+    const userData: User = readUserData(cookies)
+    console.log(userData)
     if (!userData) {
-        return
+        return null
     }
+
+    const userInfo = await getUserInfo(userData.id)
+    if (userInfo == null) {
+        return { userInfo: null, user: userData }
+    }
+
+    return { userInfo: userInfo, user: userData }
 }
+
+export const actions = {
+    insert: async ({ request, cookies }) => {
+        console.log("AA")
+    },
+    update: async ({ request, cookies }) => {
+
+    }
+
+} satisfies Actions
--- src/routes/app/user/+page.svelte
+++ src/routes/app/user/+page.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
+    // const userData = $
+    import type { PageData } from "./$types";
+    import { enhance } from "$app/forms";
+    let { data }: { data: PageData } = $props();
+    console.log(data);
 </script>
 
 User Settings
 <div></div>
-<form></form>
+
+Edit Info
+<form method="POST" use:enhance action="?/insert">
+    <input name="firstName" class="input" />
+    <input name="lastName" class="input" />
+    <button class="btn">Update</button>
+</form>
