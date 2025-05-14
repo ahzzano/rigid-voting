706a15ca0af04e0ed7cb2b7d06423a8258a66bdf
Will Santos <w.santos.gg@gmail.com>
2025-05-14 22:34:08 +0800

    feat: add zod schemas

--- src/lib/zodSchemas.ts
+++ src/lib/zodSchemas.ts
@@ -4,3 +4,11 @@ export const UserSchema = z.object({
     email: z.string().email({ message: "Invalid email address" }),
     password: z.string(),
 });
+
+export const QuestionSchema = z.object({
+    text: z.string().nonempty({ message: "No Empty Question" }),
+})
+
+export const ChoiceSchema = z.object({
+    content: z.string().nonempty({ message: "No Empty Choice" })
+})
